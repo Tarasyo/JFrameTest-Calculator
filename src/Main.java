5ba62e7c1ec26cd78913fdca8b37c992a039741d
Taras Boreyko <tarasyo7@gmail.com>
2018-10-08 14:53:55 +0100

    Add responed from text fielad on the buttons and adding function

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
 	
+	int sum;
+	int num1;
+	int num2;
 	
-
+	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -29,10 +37,10 @@ public class Main extends JFrame {
 		//text field settings
 		JTextField myTF = new JTextField(45);
 		this.add(myTF);
+		myTF.setEditable(false);
 		myTF.setPreferredSize(new Dimension(500, 100));
         myTF.setMaximumSize(new Dimension(800, 300));
 		myTF.setMinimumSize(new Dimension(250, 50));
-		myTF.setText("People like numbers");
 	
 		//Panel and its settings
 		GridLayout manager2 = new GridLayout(5, 4);
@@ -46,55 +54,76 @@ public class Main extends JFrame {
 		this.add(myPanel);
 		
 		
-		//First row
-		JButton myButton = new JButton("CE");
-		myPanel.add(myButton);
-		JButton myButton2 = new JButton("C");
-		myPanel.add(myButton2);
-		JButton myButton3 = new JButton("<=");
-		myPanel.add(myButton3);
-		JButton myButton4 = new JButton("÷");
-		myPanel.add(myButton4);
+		
+		ArrayList <JButton> buttons = new ArrayList<>();
+		buttons.add(new JButton("CE"));
+		buttons.add(new JButton("C"));
+		buttons.add(new JButton("<="));
+		buttons.add(new JButton("÷"));
 		
 		//Second row
-		JButton myButton5 = new JButton("7");
-		myPanel.add(myButton5);
-		JButton myButton6 = new JButton("8");
-		myPanel.add(myButton6);
-		JButton myButton7 = new JButton("9");
-		myPanel.add(myButton7);
-		JButton myButton8 = new JButton("x");
-		myPanel.add(myButton8);
+		buttons.add(new JButton("7"));
+		buttons.add(new JButton("8"));
+		buttons.add(new JButton("9"));
+		buttons.add(new JButton("*"));
+		
 		
 		//third row
-		JButton myButton9 = new JButton("4");
-		myPanel.add(myButton9);
-		JButton myButton10 = new JButton("5");
-		myPanel.add(myButton10);
-		JButton myButton11 = new JButton("6");
-		myPanel.add(myButton11);
-		JButton myButton12 = new JButton("-");
-		myPanel.add(myButton12);
+		buttons.add(new JButton("4"));
+		buttons.add(new JButton("5"));
+		buttons.add(new JButton("6"));
+		buttons.add(new JButton("-"));
 		
 		//Fourth row 
-		JButton myButton13 = new JButton("1");
-		myPanel.add(myButton13);
-		JButton myButton14 = new JButton("2");
-		myPanel.add(myButton14);
-		JButton myButton15 = new JButton("3");
-		myPanel.add(myButton15);
-		JButton myButton16 = new JButton("+");
-		myPanel.add(myButton16);
+		buttons.add(new JButton("1"));
+		buttons.add(new JButton("2"));
+		buttons.add(new JButton("3"));
+		buttons.add(new JButton("+"));
+		
 		
 		//Fifth row
-		JButton myButton17 = new JButton("+/-");
-		myPanel.add(myButton17);
-		JButton myButton18 = new JButton("0");
-		myPanel.add(myButton18);
-		JButton myButton19 = new JButton(".");
-		myPanel.add(myButton19);
-		JButton myButton20 = new JButton("=");
-		myPanel.add(myButton20);
+		buttons.add(new JButton("+/-"));
+		buttons.add(new JButton("0"));
+		buttons.add(new JButton("."));
+		buttons.add(new JButton("="));
+		
+		
+		
+		for(JButton button : buttons) {
+			myPanel.add(button);
+			
+			button.addActionListener(new ActionListener() {
+			
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+					if(button.getText().equals("+")) {
+						
+						num1 = Integer.valueOf(myTF.getText());
+						System.out.println(num1);
+						myTF.setText("");
+
+					}else if(button.getText().equals("=")) {
+						num2 = Integer.valueOf(myTF.getText());
+						System.out.println(num2);
+						myTF.setText("");
+						sum = num1 + num2;
+						System.out.println(sum);
+						myTF.setText(String.valueOf(sum));
+					}else{
+						myTF.setText(myTF.getText()+button.getText());
+						
+					}
+				
+					
+					
+				}});
+		}
+		
+		
+		
+		
 		
 		this.validate();
 		this.repaint();
